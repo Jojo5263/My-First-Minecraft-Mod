39b9d0608f2736f9c1b8f30f40628a73dfc125d8
Jojo5263 <122172862+Jojo5263@users.noreply.github.com>
2025-02-17 14:30:17 +0100

    Alexandrite

--- src/main/java/net/jojo/tutorialmod/TutorialMod.java
+++ src/main/java/net/jojo/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.jojo.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.jojo.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -36,7 +38,7 @@ public class TutorialMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        ModItems.register(modEventBus);
 
 
 
@@ -55,7 +57,10 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
