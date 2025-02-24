ab4e9cdfa9802d06e89b62b8f6ec90fdcef17d49
Jojo5263 <122172862+Jojo5263@users.noreply.github.com>
2025-02-24 17:07:47 +0100

    Loottable

---
+++ src/main/java/net/jojo/tutorialmod/item/custom/ChiselItem.java
@@ -0,0 +1,11 @@
+package net.jojo.tutorialmod.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class ChiselItem extends Item {
+    public ChiselItem(Properties properties) {
+        super(properties);
+    }
+
+
+}
